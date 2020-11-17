6902484625c6d1a7ecfda4d806b89b4557083d61
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2020-11-17 12:54:23 +0100

    implemented handPose

--- mySketch1.js
+++ mySketch1.js
@@ -1,17 +1,19 @@
 let video;
-let poseNet;
-let poses = [];
+let handPose;
+let predictions = [];
 
 //standard p5 structure
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(width, height);
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on("pose", function(results) {
-    poses = results;
+  handPose = ml5.handpose(video, modelLoaded);
+  handPose.on("predict", function(results) {
+    predictions = results;
   });
   video.hide();
+  noStroke();
+  fill(245,245,245,150);
 }
 
 
@@ -19,7 +21,7 @@ function draw(){
     background(255)
     translate(width,0)
     scale(-1,1);
-    tint(255,128)
+    //tint(255,128)
     image(video, 0, 0, width, height);
     drawPoints();   
 }
@@ -30,17 +32,12 @@ const modelLoaded = () => {
   console.log('Model should be loaded')
 }
 
-const drawPoints = () => {
+function drawPoints (){
   // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    const pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j += 1) {
-      const keypoint = pose.keypoints[j];
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
+  if(predictions.length>0){
+      let c =0;
+    for(let i=0; i<predictions[0]['landmarks'].length; i++){
+        ellipse(predictions[0]['landmarks'][i][0], predictions[0]['landmarks'][i][1],2*(predictions[0]['landmarks'][i][2]*(-1)),2*(predictions[0]['landmarks'][i][2]*(-1)));
     }
   }
 }
